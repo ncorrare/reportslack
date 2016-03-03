8db99ff0a520cb19da86be9f3de22c819fb03dfa
Nicolas Corrarello <ncorrare@gmail.com>
2016-03-03 12:48:23 +0000

    Trying to get Coveralls and simplecov to play along

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'coveralls'
 require 'simplecov'
+require 'coveralls'
 #Coveralls.wear!
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
 	  add_filter 'spec/fixtures/modules/'
 end
-Coveralls.wear!
 at_exit { RSpec::Puppet::Coverage.report! }
