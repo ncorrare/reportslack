8ee7e1cec1d7177037cba6c0a9d5de7dbada276d
Nicolas Corrarello <ncorrare@gmail.com>
2016-03-04 10:20:07 +0000

    Gave up on Simplecov after talking to garethr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,4 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'simplecov'
 require 'coveralls'
-#Coveralls.wear!
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.start do
-	  add_filter 'spec/fixtures/modules/'
-end
+Coveralls.wear!
