5d489f8f6582dbf77f92ef9e9b3192c472b33835
Nicolas Corrarello <ncorrare@gmail.com>
2016-03-31 17:14:20 +0100

    Added test for PR#6

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -33,6 +33,39 @@ describe 'reportslack', :type => :class do
 			}).that_requires('Ini_Setting[enable_reports]')
 		}
   end
+  describe 'with incoming webhook app' do
+                let (:params) {  {
+                        :webhook => 'https://hooks.slack.com/services/TXXXXX/BXXXXX/XXXXXXXXXX',
+                        :channel => '#default',
+                } }
+		it { 
+			should compile
+			should create_class('reportslack') 
+			should contain_package('slack-notifier').with ({
+				'ensure' => 'latest',
+				'provider' => 'puppetserver_gem',
+			})
+			should contain_file('/etc/puppet/slack.yaml').with ({
+				'content' => "---\nwebhook_url: \"https://hooks.slack.com/services/TXXXXX/BXXXXX/XXXXXXXXXX\"\nchannel: \"#default\"\n",
+				'owner' => 'pe-puppet',
+				'mode' => '0644',
+				'group' => 'pe-puppet',
+			})
+			should contain_ini_setting('enable_reports').with({
+				'section' => 'agent',
+				'setting' => 'report',
+				'value' => 'true',
+				'path' => '/etc/puppet/puppet.conf',
+			})
+			should contain_ini_subsetting('slack_report_handler').with({
+				'section' => 'master',
+				'setting' => 'reports',
+				'subsetting' => 'slack',
+				'subsetting_separator' => ',',
+				'path' => '/etc/puppet/puppet.conf',
+			}).that_requires('Ini_Setting[enable_reports]')
+		}
+  end
 
   describe 'without parameters' do
 	  it { should_not compile }
