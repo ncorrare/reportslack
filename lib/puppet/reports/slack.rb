59801f1e6bd2b768f142eeee3ad655045efa77ee
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2017-05-24 23:36:50 +0200

    add SLACK_ICON and SLACK_USERNAME
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- lib/puppet/reports/slack.rb
+++ lib/puppet/reports/slack.rb
@@ -13,7 +13,10 @@ Puppet::Reports.register_report(:slack) do
 	config = YAML.load_file(configfile)
 	SLACK_WEBHOOK_URL = config['webhook_url']
   Puppet.debug "Webhook is #{SLACK_WEBHOOK_URL}"
-	SLACK_CHANNEL = config['channel']
+	SLACK_CHANNEL  = config['channel']
+  SLACK_ICON     = config['icon_url']
+  SLACK_USERNAME = config['username']
+  # "https://puppetlabs.com/wp-content/uploads/2010/12/PL_logo_vertical_RGB_lg.jpg"
 	PUPPETCONSOLE = config['puppetconsole']
 	DISABLED_FILE = File.join([File.dirname(Puppet.settings[:config]), 'slack_disabled'])
 
@@ -23,8 +26,8 @@ Puppet::Reports.register_report(:slack) do
 		if (!disabled && self.status != 'unchanged')
 			Puppet.debug "Sending notification for #{self.host} to Slack channel #{SLACK_CHANNEL}"
 			msg = "Puppet run for #{self.host} #{self.status} at #{Time.now.asctime} on #{self.configuration_version} in #{self.environment}. Report available <https://#{PUPPETCONSOLE}/#/node_groups/inventory/node/#{self.host}/reports|here>"
-			notifier = Slack::Notifier.new SLACK_WEBHOOK_URL, channel: SLACK_CHANNEL, username: 'Puppet'
-			notifier.ping msg, icon_url: "https://puppetlabs.com/wp-content/uploads/2010/12/PL_logo_vertical_RGB_lg.jpg"
+			notifier = Slack::Notifier.new SLACK_WEBHOOK_URL, channel: SLACK_CHANNEL, username: SLACK_USERNAME
+			notifier.ping msg, icon_url: SLACK_ICON
 		end
 	end
 end
