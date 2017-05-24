d96e11d7be6ebb8dbaea575299ae7477dac0c94d
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2017-05-24 23:35:27 +0200

    rework main manifest and externalize params
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,48 @@
-# Authors
-# -------
+# -*- mode: puppet; -*-
+# Time-stamp: <Wed 2017-05-24 23:34 svarrette>
+# ------------------------------------------------------------------------------
+# Class reportslack
 #
-# Nicolas Corrarello <nicolas@corrarello.com>
+# @summary A custom report processor to send notifications to slack
+#
+# @param webhook [String]
+#   Slack Incoming WebHooks URL -- to be generated from your Slack team App Directory
+#   (Manage / Custom Integrations / Incoming WebHooks)
+# @param channel [String] Default: '#general'
+#   Default channel to send messages to
+# @param puppetconsole [String]
+# @param username [String] Default: 'Puppet'
+#   Username that this integration will post as.
+# @param icon_url [String] Default: wikipedia logo
+#   Icon that is used for messages from this integration.
+#
+# @author Nicolas Corrarello <nicolas@corrarello.com>
+#         Sebastien Varrette aka Falkor <sebastien.varrette@uni.lu>
 #
 # Copyright
 # ---------
 #
-# Copyright 2016 Nicolas Corrarello, unless otherwise noted.
+# Copyright 2016-2017 Nicolas Corrarello, unless otherwise noted.
 #
 class reportslack (
-  $webhook,
-  $channel,
-  $puppetconsole = $settings::ca_server,
-) {
+  Enum[
+    'present',
+    'absent'
+  ]      $ensure        = $reportslack::params::ensure,
+  String $webhook,
+  String $channel       = $reportslack::params::channel
+  String $puppetconsole = $settings::ca_server,
+  String $username      = $reportslack::params::username,
+  String $icon_url      = $reportslack::params::icon_url
+)
+inherits reportslack::params
+{
   validate_re($webhook, 'https:\/\/hooks.slack.com\/(services\/)?T.+\/B.+\/.+', 'The webhook URL is invalid')
   validate_re($channel, '#.+', 'The channel should start with a hash sign')
 
   package { 'slack-notifier':
     ensure   => latest,
+    name     => $reportslack::params::packagename,
     provider => 'puppetserver_gem'
   }
 
@@ -30,7 +55,7 @@ class reportslack (
   }
 
   ini_subsetting { 'slack_report_handler':
-    ensure               => present,
+    ensure               => $ensure,
     path                 => "${settings::confdir}/puppet.conf",
     section              => 'master',
     setting              => 'reports',
@@ -39,11 +64,19 @@ class reportslack (
     require              => Ini_setting['enable_reports'],
   }
 
+  if defined(Package['pe-puppet']) {
+    $owner = 'pe-puppet'
+    $group = 'pe-puppet'
+  }
+  else {
+    $owner = $reportslack::params::configfile_owner
+    $group = $reportslack::params::configfile_group
+  }
   file { "${settings::confdir}/slack.yaml":
-    ensure  => present,
-    owner   => 'pe-puppet',
-    group   => 'pe-puppet',
-    mode    => '0644',
+    ensure  => $ensure,
+    owner   => $owner,
+    group   => $group,
+    mode    => $reportslack::params::configfile_mode,
     content => template('reportslack/slack.yaml.erb'),
     require => Package['slack-notifier'],
   }
---
+++ manifests/params.pp
@@ -0,0 +1,54 @@
+# -*- mode: puppet; -*-
+# Time-stamp: <Wed 2017-05-24 23:33 svarrette>
+# ------------------------------------------------------------------------------
+# = Class: reportslack::params
+#
+# In this class are defined as variables values that are used in other
+# reportslack classes.
+# This class should be included, where necessary, and eventually be enhanced
+# with support for more OS
+#
+# == Warnings
+#
+# /!\ Always respect the style guide available
+# here[http://docs.puppetlabs.com/guides/style_guide]
+#
+# The usage of a dedicated param classe is advised to better deal with
+# parametrized classes, see
+# http://docs.puppetlabs.com/guides/parameterized_classes.html
+#
+# [Remember: No empty lines between comments and class definition]
+#
+class reportslack::params {
+
+  ######## DEFAULTS FOR VARIABLES USERS CAN SET ##########################
+  # (Here are set the defaults, provide your custom variables externally)
+  # (The default used is in the line with '')
+  ###########################################
+
+  # ensure the presence (or absence) of reportslack
+  $ensure = 'present'
+  $channel  = '#general'
+  $username = 'Puppet'
+  $icon_url = 'https://upload.wikimedia.org/wikipedia/en/0/09/Puppet%27s_company_logo.png'
+
+  #### MODULE INTERNAL VARIABLES  #########
+  # (Modify to adapt to unsupported OSes)
+  #######################################
+  $packagename = $::operatingsystem ? {
+    default => 'slack-notifier',
+  }
+
+  $configfile_mode = $::operatingsystem ? {
+    default => '0440',
+  }
+
+  $configfile_owner = $::operatingsystem ? {
+    default => 'puppet',
+  }
+
+  $configfile_group = $::operatingsystem ? {
+    default => 'puppet',
+  }
+
+}
