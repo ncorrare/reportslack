46294c34535ec572ab7e827392ac10d40cf56983
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2017-05-27 16:41:49 +0200

    variable call update
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- lib/puppet/reports/slack.rb
+++ lib/puppet/reports/slack.rb
@@ -6,29 +6,29 @@ Puppet::Reports.register_report(:slack) do
   desc "A custom reports processor to send notifications to a Slack channel"
 
   if (Puppet.settings[:config]) then
-    configfile = File.join([File.dirname(Puppet.settings[:config]), "slack.yaml"])
+    @configfile = File.join([File.dirname(Puppet.settings[:config]), "slack.yaml"])
   else
-    configfile = "/etc/puppetlabs/puppet/slack.yaml"
+    @configfile = "/etc/puppetlabs/puppet/slack.yaml"
   end
-  Puppet.debug "Reading #{configfile}"
-  raise(Puppet::ParseError, "Slack report config file #{configfile} not readable") unless File.exist?(configfile)
-  config = YAML.load_file(configfile)
+  Puppet.debug "Reading #{@configfile}"
+  raise(Puppet::ParseError, "Slack report config file #{@configfile} not readable") unless File.exist?(@configfile)
+  @config = YAML.load_file(@configfile)
 
   options = {
-    webhook_url: config['webhook_url'],
-    channel:     config['channel']
+    webhook_url: @config['webhook_url'],
+    channel:     @config['channel']
   }
   ['username', 'icon_url', 'icon_emoji', 'link_names'].each do |k|
-    options[k] = config[k] if config[k]
+    options[k] = @config[k] if @config[k]
   end
-  PUPPETCONSOLE = config['puppetconsole']
-  DISABLED_FILE = File.join([File.dirname(Puppet.settings[:config]), 'slack_disabled'])
+  PUPPETCONSOLE = @config['puppetconsole']
+  DISABLED_FILE = File.join([File.dirname(Puppet.settings[:@config]), 'slack_disabled'])
 
   def process
     disabled = File.exists?(DISABLED_FILE)
 
     if (!disabled && self.status != 'unchanged')
-      Puppet.debug "Sending notification for #{self.host} to Slack channel #{config['channel']}"
+      Puppet.debug "Sending notification for #{self.host} to Slack channel #{@config['channel']}"
       msg = "Puppet run for #{self.host} #{self.status} at #{Time.now.asctime} on #{self.configuration_version} in #{self.environment}. Report available <https://#{PUPPETCONSOLE}/#/node_groups/inventory/node/#{self.host}/reports|here>"
       notifier = Slack::Notifier.new(options)
       notifier.notify(msg)
